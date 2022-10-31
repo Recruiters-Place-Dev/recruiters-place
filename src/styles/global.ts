b2ed88adff7ce8f271abe122ac2f58a81fcfb811
pabloaraujo <pabloaraujo_engmec@hotmail.com>
2022-10-31 19:08:10 -0300

    feat(all): add libs react-toastify, yup, framer-motion, axios, uuid, react-icons, styled-components, react-hook-form, react-router-dom

---
+++ src/styles/global.ts
@@ -0,0 +1,10 @@
+import { createGlobalStyle } from "styled-components";
+
+export const Global = createGlobalStyle`
+    *{
+        margin: 0px;
+        padding: 0px;
+        list-style: none;
+        box-sizing: border-box
+    }
+`;
