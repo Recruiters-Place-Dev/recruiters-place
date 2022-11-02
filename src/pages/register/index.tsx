d19640fb7d4e49d9846c1be54ee130c14154b053
Igorttdp <igorttdp.dev@gmail.com>
2022-11-02 13:05:39 -0300

    chore: add html structure

--- src/pages/register/index.tsx
+++ src/pages/register/index.tsx
@@ -1,12 +1,21 @@
 import React from "react";
-import { FormRegister } from "../../components/formregister"
+import { FormRegister } from "../../components/formregister";
+import RegisterContainer from "./RegisterContainer";
 
 function Register() {
   return (
-    <div>
-      Register
-      <FormRegister />
-    </div>
+    <RegisterContainer>
+      <div className="FormContainer">
+        <p>Logo</p>
+        <h2>Cadastrar</h2>
+        <FormRegister />
+        <div className="BackToLogin">
+          <span>Já possuí uma conta ?</span>
+          <button>Login</button>
+        </div>
+      </div>
+      <div className="ContentContainer"></div>
+    </RegisterContainer>
   );
 }
 
