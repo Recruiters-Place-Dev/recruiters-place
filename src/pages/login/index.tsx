ba76211116c2c066b197289db3f3608f61272f9d
Igorttdp <igorttdp.dev@gmail.com>
2022-11-02 14:56:44 -0300

    fix: fix type problems

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -44,7 +44,7 @@ export const Login = () => {
           </figure>
           <FormLogin onSubmit={handleSubmit(onLogin)}>
             <h3>Entrar</h3>
-            <Input label="Email" type="text" register={register} id="email" />
+            <Input label="Email" type="text" register={register} id="email" login={true} />
             {
               <p className="errors">
                 {errors.email && <RiErrorWarningFill />}
@@ -56,6 +56,7 @@ export const Login = () => {
               type="password"
               register={register}
               id="password"
+              login={true}
             />
             {
               <p className="errors">
