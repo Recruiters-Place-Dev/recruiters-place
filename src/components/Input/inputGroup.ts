1155741a72bcec94a9563cbdce4f617516889c12
Igorttdp <igorttdp.dev@gmail.com>
2022-11-02 09:53:49 -0300

    fix: fix label positioning when input is filled

--- src/components/Input/inputGroup.ts
+++ src/components/Input/inputGroup.ts
@@ -8,14 +8,15 @@ interface iInputGroup {
 
 const InputGroup = styled.div<iInputGroup>`
     position: relative;
+    width: 100%;
 
     input {
-        width: 400px;
+        width: 100%;
         border: 1px solid ${props => props.className === "error" ? "#E2142D" : props.className === "sucess" ? "#4BAD31" : props.className === "done" ? "#1DA1F2" : "#14171A"};
         border-radius: 8px;
         background: none;
         color: ${props => props.className === "sucess" ? "#4BAD31" : props.className === "done" ? "#1DA1F2" : "#14171A"};
-        padding: 17px 14px;
+        padding: 16.5px 14px;
         font-size: 16px;
         transition: 150ms cubic-bezier(0.4, 0, 0.2, 1);
     }
@@ -25,7 +26,9 @@ const InputGroup = styled.div<iInputGroup>`
         left: 16px;
         pointer-events: none;
         color: ${props => props.className === "error" ? "#E2142D" : props.className === "sucess" ? "#4BAD31" : props.className === "done" ? "#1DA1F2" : "#14171A"};;
-        transform: translateY(1rem);
+        transform: ${props => props.inputValue === "" ? "translateY(1rem)" : " translateY(-50%) scale(0.8)"};
+        background-color: ${props => props.inputValue !== "" ? "#fff" : "transparent"};
+        padding: ${props => props.inputValue !== "" ? "0 0.2em" : 0};
         transition: 150ms cubic-bezier(0.4, 0, 0.2, 1);
     }
 
