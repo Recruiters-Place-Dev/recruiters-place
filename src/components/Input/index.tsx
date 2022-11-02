64064eb5cf649d3814a1ece5335b5f38c9804022
Igorttdp <igorttdp.dev@gmail.com>
2022-11-02 13:09:33 -0300

    feat: add hook form register function by props| add rules for input className

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,23 +1,41 @@
 import { useState } from "react";
+import { Path, UseFormRegister } from "react-hook-form";
 import InputGroup from "./inputGroup";
+import { iUserRegister } from "../formregister/index";
 
 interface iInputProps {
-  className?: string;
   type: string;
   label: string;
+  id: Path<iUserRegister>;
+  register: UseFormRegister<iUserRegister>;
+  errors?: any;
+  login?: boolean
 }
 
-const Input = ({ className, type, label }: iInputProps) => {
+const Input = ({ type, label, id, register, errors, login }: iInputProps) => {
   const [value, setValue] = useState("");
 
+  const { onChange, onBlur, name, ref } = register(id);
+
   return (
-    <InputGroup className={className} inputValue={value}>
+    <InputGroup
+      className={!errors && value !== "" && login ? "done" : errors ? "error" : !errors && value !== "" ? "sucess" : ""}
+      inputValue={value}
+    >
       <input
+        autoComplete="off"
+        id={id}
         value={value}
-        onChange={(e) => setValue(e.target.value)}
         type={type}
+        onChange={(e) => {
+          setValue(e.target.value);
+          onChange(e);
+        }}
+        onBlur={onBlur}
+        name={name}
+        ref={ref}
       />
-      <label htmlFor="">{label}</label>
+      <label>{label}</label>
     </InputGroup>
   );
 };
