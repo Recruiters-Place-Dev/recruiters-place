9f9e0f0b7da2317566e9aad39bfec40112979fe7
Matheus M <matheus150101miranda@gmail.com>
2022-11-04 13:27:12 -0300

    feat(Login): add style in Header and fix gambiarra

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,11 @@
 import axios from "axios";
 
-export const Api = axios.create({
+const token = localStorage.getItem("RPlace:Token");
+
+export const Api = axios.create({    
     baseURL: 'http://localhost:3001',
-    timeout: 2500
+    timeout: 2500,
+    headers: {
+        "Authorization": `Bearer ${token}`
+    }
 });
