0fba16b7def927c61cc420a0ea4a6a235ab9470b
pabloaraujo <pabloaraujo_engmec@hotmail.com>
2022-11-04 15:36:08 -0300

    feat(coments): add data onsubmit

--- src/components/formMessage/index.tsx
+++ src/components/formMessage/index.tsx
@@ -1,33 +1,49 @@
 import React, { useContext } from "react";
 import { useForm } from "react-hook-form";
 import { iUser, WebContext } from "../../context/webcontext";
-import Input from "../Input";
 import { ButtonSendComent, FormContainer } from "./style";
 
 function FormMessage() {
   const { modalWriteComent, setModalWriteComent, user, comentId, allUsers } =
     useContext(WebContext);
-  const { register, handleSubmit } = useForm({});
+  const { register, handleSubmit } = useForm<iComent>({});
+
+  function onSubmitComent(data: iComent) {
+    data.idTo = comentId;
+    data.idFrom = String(user?.id);
+    console.log(data);
+  }
+
+  interface iComent {
+    from: string | undefined;
+    to: string | undefined;
+    idTo: string | undefined;
+    idFrom: string | undefined;
+    coment: string | undefined;
+  }
+
   return (
-    <FormContainer>
+    <FormContainer onSubmit={handleSubmit(onSubmitComent)}>
       <div>
         <h3>De: </h3>
-        <h1>{user?.name}</h1>
+        <input type="text" value={user?.name} {...register("from")} />
       </div>
       <div>
         <h3>Para:</h3>
-        <h1>
-          {allUsers?.reduce((acc, act: iUser) => {
+        <input
+          type="text"
+          value={allUsers?.reduce((acc, act: iUser) => {
             if (act.id === Number(comentId)) {
               return acc + act.name;
             } else {
               return acc + "";
             }
-          }, " ")}
-        </h1>
+          }, "")}
+          {...register("to")}
+        />
       </div>
-      <textarea name="" id=""></textarea>
-      <ButtonSendComent>Enviar</ButtonSendComent>
+      <textarea id="" {...register("coment")}></textarea>
+      <ButtonSendComent type="submit">Enviar</ButtonSendComent>
     </FormContainer>
   );
 }
--- src/components/formMessage/style.ts
+++ src/components/formMessage/style.ts
@@ -4,6 +4,9 @@ export const FormContainer = styled.form`
   display: flex;
   flex-direction: column;
   gap: 15px;
+  input {
+    font-size: 24px;
+  }
 
   textarea {
     height: 200px;
