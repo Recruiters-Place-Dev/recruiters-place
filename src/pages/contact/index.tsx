f4d89f59120ae6e3f39de1ba69a87397e30b5fe1
pabloaraujo <pabloaraujo_engmec@hotmail.com>
2022-11-06 11:25:43 -0300

    feat(contact): add contact

--- src/pages/contact/index.tsx
+++ src/pages/contact/index.tsx
@@ -1,23 +1,51 @@
-import React from "react";
+import React, { useRef } from "react";
 import { useForm } from "react-hook-form";
 import Input from "../../components/Input";
 import { ContainerFormContact, ContainerImg, MainContainer } from "./style";
 import Logo from "../../assets/Logo.svg";
 import { ButtonStylized } from "../../components/buttons/style";
+import emailjs from "@emailjs/browser";
+
 function ContactUs() {
   const { register, handleSubmit, getValues } = useForm({});
+
+  const form = useRef<HTMLFormElement | null>(null);
+
+  const sendEmail = (e: any) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_4xcvgit",
+        "template_i8zgodl",
+        form.current as HTMLFormElement | string,
+        "b0jyyoI0tatXmI68y"
+      )
+      .then(
+        (result) => {
+          e.target.reset();
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+
   return (
     <MainContainer>
       <ContainerImg>
         <img src={Logo} alt="" />
+        <h1> vs beta</h1>
       </ContainerImg>
-      <ContainerFormContact>
+      <ContainerFormContact ref={form} onSubmit={sendEmail}>
         <Input
           type="text"
           label="Nome"
           id="name"
           register={register}
           getValues={getValues}
+          name="name"
         />
         <Input
           type="text"
@@ -25,6 +53,7 @@ function ContactUs() {
           id="email"
           register={register}
           getValues={getValues}
+          name="email"
         />
         <Input
           type="text"
@@ -32,9 +61,10 @@ function ContactUs() {
           id="reason"
           register={register}
           getValues={getValues}
+          name="reason"
         />
 
-        <textarea name="" id=""></textarea>
+        <textarea name="message" id=""></textarea>
         <ButtonStylized>Enviar</ButtonStylized>
       </ContainerFormContact>
     </MainContainer>
