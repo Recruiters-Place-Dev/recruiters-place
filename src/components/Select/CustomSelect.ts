99722eb5b483aabab7b1816728d87963fac17209
Igorttdp <igorttdp.dev@gmail.com>
2022-11-07 12:09:37 -0300

    feat + style: add new styled-component

---
+++ src/components/Select/CustomSelect.ts
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+const CustomSelect = styled.div`
+  width: 100%;
+  position: relative;
+
+  select {
+    width: 100%;
+    border: 1px solid #14171a;
+    border-radius: 8px;
+    padding: 16.5px 14px;
+    font-size: 16px;
+  }
+`;
+
+export default CustomSelect;
