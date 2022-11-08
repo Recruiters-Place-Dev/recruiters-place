78a2af60af6cf693b0f6f1e58c11081a8279b48e
Igorttdp <igorttdp.dev@gmail.com>
2022-11-08 13:33:52 -0300

    feat: add hamburguer menu component & add open and close functions

---
+++ src/components/hamburguerMenu/index.tsx
@@ -0,0 +1,35 @@
+import MenuContainer from "./hambuguerMenu";
+import Logo from "../../assets/Logo.svg";
+import { useState } from "react";
+
+const HamburguerMenu = () => {
+    const [className, setClassName] = useState("Controls")
+
+    const Menu = () => {
+        if (className === "Controls") {
+            setClassName("Controls active")
+        } else setClassName("Controls")
+    }
+
+  return (
+    <MenuContainer>
+      <nav id="navbar">
+        <div className="NavContainer">
+          <div className="MenuContainer">
+            <img id="NavBrand" src={Logo} alt="" />
+            <button id="MenuBtn" onClick={Menu}></button>
+          </div>
+          <div className="ControlsContainer">
+            <div className={className}>
+              <button>Botão</button>
+              <button>Botão</button>
+              <button>Botão</button>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </MenuContainer>
+  );
+};
+
+export default HamburguerMenu;
