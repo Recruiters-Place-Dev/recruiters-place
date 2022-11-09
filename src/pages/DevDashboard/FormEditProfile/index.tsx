485b05ae997a921425cda2ccefe59156139c3142
pabloaraujo <pabloaraujo_engmec@hotmail.com>
2022-11-09 10:54:38 -0300

    feat(editperfil): fix exit

--- src/pages/DevDashboard/FormEditProfile/index.tsx
+++ src/pages/DevDashboard/FormEditProfile/index.tsx
@@ -3,17 +3,21 @@ import { BoxForm } from "./style";
 import { useState } from "react";
 import { FormTechOfDev } from "./FormTechOfDev";
 import { FormDataOfDev } from "./FormDataOfDev";
+import { LogOffModal } from "../../../components/logoff";
 
 export const FormEditProfile = () => {
   const [step, setStep] = useState<1 | 2>(1);
 
   return (
-    <BoxForm step={step}>
-      {step === 1 ? (
-        <FormDataOfDev setStep={setStep} />
-      ) : (
-        <FormTechOfDev setStep={setStep} />
-      )}
-    </BoxForm>
+    <>
+      <BoxForm step={step}>
+        {step === 1 ? (
+          <FormDataOfDev setStep={setStep} />
+        ) : (
+          <FormTechOfDev setStep={setStep} />
+        )}
+      </BoxForm>
+      <LogOffModal />
+    </>
   );
 };
