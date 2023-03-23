ac1d3c6a77b13677301b82f7bb92df674b456c4b
Igorttdp <igorttdp.dev@email.com>
2023-03-23 11:22:28 -0300

    feat: add new api path

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 const token = localStorage.getItem("RPlace:Token");
 
 export const Api = axios.create({
-  baseURL: "http://localhost:3001/",
+  baseURL: "https://recruiters-place-fake-api.onrender.com",
   timeout: 2500,
   headers: {
     Authorization: `Bearer ${token}`,
